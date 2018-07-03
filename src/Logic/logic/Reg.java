4ac35b4cd11ec069fa1b1c982bda5c8c73380af3
Chamith-Nimmitha <Chamith Nimmitha>
2018-07-03 17:21:36 +0530

    ad

---
+++ src/Logic/logic/Reg.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic.logic;
+
+/**
+ *
+ * @author chamith
+ */
+public  class Reg{
+  
+    //Declare static variables for transfer data
+        static int campaign_id=0;
+        static String campaign_name = null;
+        static String fb_link = null;
+        static String twitter_link = null;
+        static String telegram_link = null;
+        static String start_date = null;
+        static String end_date = null;
+        static int token_price = 0;
+        static int complete_fb = 0;
+        static int complete_twitter = 0;
+        static int complete_telegram=0;
+        static int recommend_fb = 0;
+        static int recommend_twitter  = 0;
+        static int recommend_telegram =0;
+        static int week_number = 0;
+        
+}
+    
+
